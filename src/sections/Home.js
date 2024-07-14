16502c3aa0a443f6ce4e9bc80426588ad93369a8
Garrett Strange <garrett.strange@yahoo.com>
2024-07-14 18:08:51 -0400

    Updated commit

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import '../styles/Home.css';
 import TypingText from '../components/TypingTextThird.js';
 import AsciiWords from '../components/AsciiWords.js';
-import imagine from '../assets/download.jpg';
 
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <TypingText delayTime={10}>
-          <p className="greeting-text">Hello, my name is</p>
-          <div className="ascii-wrapper">
+        <TypingText delayTime={30}>
+          <div className="text-container">
+            <p className="greeting-text">Hello, my name is</p>
+          </div>
+          <div className="ascii-container">
             <AsciiWords keyName="Garrett Strange" className="Garrett" />
           </div>
         </TypingText>
