57ebd47b0d010841f0ee0c6f4cc04b69ff7340bf
Garrett Strange <garrett.strange@yahoo.com>
2024-07-14 05:31:14 -0400

    Updated commit

--- src/components/TypingText.js
+++ src/components/TypingText.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import '../styles/TypingText.css'; // Ensure you have the correct path to your CSS file
+import '../styles/TypingText.css';
 
 const TypingText = ({ children, delayTime = 30 }) => {
   const originalText = useRef([]);
@@ -17,7 +17,6 @@ const TypingText = ({ children, delayTime = 30 }) => {
       }
     });
 
-    // Move the cursor to the beginning element and add a █ character
     if (elements.current.length > 0 && cursor.current) {
       const firstElement = elements.current[0];
       if (firstElement && firstElement.nodeType === Node.ELEMENT_NODE) {
@@ -40,7 +39,6 @@ const TypingText = ({ children, delayTime = 30 }) => {
         element.style.visibility = 'visible';
       }
     }
-    // Move the cursor to the last element
     if (elements.current.length > 0 && cursor.current) {
       const lastElement = elements.current[elements.current.length - 1];
       if (lastElement && lastElement.nodeType === Node.ELEMENT_NODE) {
@@ -57,14 +55,14 @@ const TypingText = ({ children, delayTime = 30 }) => {
       let charIndex = 0;
       const interval = setInterval(() => {
         if (cursor.current && cursor.current.parentElement) {
-          cursor.current.parentElement.removeChild(cursor.current); // Remove cursor from the previous element
+          cursor.current.parentElement.removeChild(cursor.current);
         }
         element.textContent += text.charAt(charIndex);
         charIndex++;
         if (charIndex < text.length) {
           if (element && element.nodeType === Node.ELEMENT_NODE && cursor.current) {
             if (!element.contains(cursor.current)) {
-              element.appendChild(cursor.current); // Append cursor to the element
+              element.appendChild(cursor.current);
             }
           }
         }
@@ -72,22 +70,23 @@ const TypingText = ({ children, delayTime = 30 }) => {
           clearInterval(interval);
           resolve();
         }
-      }, delayTime); // Adjust typing speed here (milliseconds per character)
+      }, delayTime);
     });
   };
 
   useEffect(() => {
-    const timer1 = setTimeout(() => {
-      makeInvisible(); // Make text invisible immediately
+    /*const atimer = setTimeout(() => {
+      makeInvisible();
     }, 0);
-
-    const timer2 = setTimeout(() => {
-      typeText(); // Start typing animation after a short delay
-    }, 1); // Adjust the delay here (milliseconds)
+    */
+    makeInvisible();
+    const timer = setTimeout(() => {
+      typeText();
+    }, 1);
 
     return () => {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
+      //clearTimeout(atimer);
+      clearTimeout(timer);
     };
   }, []);
 
